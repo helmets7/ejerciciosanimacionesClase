9af7ca6f3cced592f4cedc4fb0489c5485092cf1
Nerea Soler <nereamarisa56@gmail.com>
2022-12-10 00:59:06 +0100

    6 hecho

---
+++ ej6/script.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector(".boton");
+const imgastronaut = document.querySelector(".imgastronaut");
+
+boton.addEventListener("click", () => {
+    imgastronaut.classList.toggle("animar");
+});
+
+    
