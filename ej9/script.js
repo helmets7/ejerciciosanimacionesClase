3614de662ce9a7813af8acd89e8a349d7d255ffc
Nerea Soler Benavent <nereamarisa56@gmail.com>
2022-12-10 02:05:59 +0100

    9 acabado

---
+++ ej9/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.button');
+const imgusuario = document.querySelector('.imgusuario');
+
+
+button.addEventListener('click', () => {
+    imgusuario.classList.remove('animar');
+});
